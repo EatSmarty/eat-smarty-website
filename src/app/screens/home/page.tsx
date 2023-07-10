e286bd9ff4e6eec4496856491e8a541405c2b375
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-10 22:15:16 +0330

    update indent

--- src/app/screens/home/page.tsx
+++ src/app/screens/home/page.tsx
@@ -8,19 +8,19 @@ interface pageProps {
 const page: FC<pageProps> = ({}) => {
   return( 
     <div>
-        <Home></Home>
-        <header className='flex justify-center items-center mt-7'>
+      <Home></Home>
+      <header className='flex justify-center items-center mt-7'>
         <section className='flex justify-between items-center '>
-                <div className='flex justify-center items-center mr-20'>
-                    <img className='w-10 rounded-full mr-3' src='/avatar-2023.jpg' alt="avatar" />
-                    <h4 className='mr-1'>Hey Hosein</h4>
-                    <div className='wave text-xl'>👋</div>
-                </div>
-                <a href='https://halalvar.com/' className="logo bg-[#9CCD62] text-black rounded-full p-2">
-                    Hal.
-                </a>
+          <div className='flex justify-center items-center mr-20'>
+            <img className='w-10 rounded-full mr-3' src='/avatar-2023.jpg' alt="avatar" />
+              <h4 className='mr-1'>Hey Hosein</h4>
+                <div className='wave text-xl'>👋</div>
+          </div>
+            <a href='https://halalvar.com/' className="logo bg-[#9CCD62] text-black rounded-full p-2">
+              Hal.
+            </a>
         </section>
-    </header>
+      </header>
     </div>
   )
 }
