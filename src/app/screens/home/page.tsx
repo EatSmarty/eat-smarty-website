7ee6e16189714990078a6822feb0a67929f702e7
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-10 22:01:07 +0330

    update header style

--- src/app/screens/home/page.tsx
+++ src/app/screens/home/page.tsx
@@ -9,15 +9,16 @@ const page: FC<pageProps> = ({}) => {
   return( 
     <div>
         <Home></Home>
-        <header className='flex justify-center items-center mt-10'>
+        <header className='flex justify-center items-center mt-7'>
         <section className='flex justify-between items-center'>
-                <div className='flex justify-center items-center mr-40'>
+                <div className='flex justify-center items-center mr-20'>
                     <img className='w-10 rounded-full mr-3' src='/avatar-2023.jpg' alt="avatar" />
-                    <h4>Hey Hosein</h4>
+                    <h4 className='mr-1'>Hey Hosein</h4>
+                    <div className='wave text-xl'>👋</div>
                 </div>
-                <div className="logo">
+                <a href='https://halalvar.com/' className="logo bg-[#9CCD62] text-black rounded-full p-2">
                     Hal.
-                </div>
+                </a>
         </section>
     </header>
     </div>
