78ec7ec5bdb38c346b4b963206332e83d6d9aa14
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-11 19:47:56 +0330

    create category section and upload imgs

--- src/app/screens/home/page.tsx
+++ src/app/screens/home/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useSnapCarousel } from 'react-snap-carousel';
 import Home from '@/app/page'
 import { FC } from 'react'
 
@@ -6,25 +8,42 @@ interface pageProps {
 }
 
 const page: FC<pageProps> = ({}) => {
+  const { scrollRef, pages, activePageIndex, next, prev, goTo } =
+    useSnapCarousel();
   return( 
-    <div>
+    <div className='home-screen'>
       <Home></Home>
-      <header className='flex justify-center items-center mt-7'>
+      <header className='home-header flex justify-center items-center mt-7'>
         <section className='flex justify-between items-center '>
           <div className='flex justify-center items-center mr-20'>
             <img className='w-10 rounded-full mr-3' src='/avatar-2023.jpg' alt="avatar" />
-              <h4 className='mr-1'>Hey Hosein</h4>
+              <h4 className='text-[#F6F8E2] mr-1'>Hey Hosein</h4>
                 <div className='wave text-xl'>👋</div>
           </div>
-            <a href='https://halalvar.com/' className="logo bg-[#9CCD62] text-black rounded-full p-2">
+            <a href='https://halalvar.com/' className="logo bg-[#9CCD62] text-black rounded-full p-2 text-[#F6F8E2]">
               Hal.
             </a>
         </section>
       </header>
       <main className='flex justify-center items-center'>
-        <div className=''>
-          <p className='font-light opacity-50 mt-10 mr-10'>What product are you looking for?</p>
-          <input className='w-full mt-3  rounded-full bg-[#3C3D42] outline-none p-2' type="text" />
+        <div>
+          <section className='search-box'>
+            <p className='text-[#F6F8E2] font-light opacity-50 mt-10 mr-10'>What product are you looking for?</p>
+            <input className='placeholder-white placeholder-opacity-50 w-full mt-3  rounded-full bg-[#3C3D42] outline-none p-2 pl-5' type="text" placeholder='search...'/>
+          </section>
+          <section className='category'>
+            <p className='text-[#F6F8E2] mt-10'>category</p>
+            <div className='containers flex w-100 scroll-section-outer'>
+                <div className='scroll-section-inner flex relative'>
+                  <div className='item1 w-20 bg-red-200 h-20 m-2'></div>
+                  <div className='item2 w-20 bg-red-200 h-20 m-2'></div>
+                  <div className='item3 w-20 bg-red-200 h-20 m-2'></div>
+                  <div className='item4 w-20 bg-red-200 h-20 m-2'></div>
+                  <div className='item5 w-20 bg-red-200 h-20 m-2'></div>
+                  <div className='item6 w-20 bg-red-200 h-20 m-2'></div>
+                </div>
+            </div>
+          </section>
         </div>
       </main>
     </div>
