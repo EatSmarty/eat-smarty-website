6042e625bf82228ae7188fb8c506893eca2460e7
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-13 16:43:26 +0330

    set rounded to border radius

--- src/app/screens/home/page.tsx
+++ src/app/screens/home/page.tsx
@@ -27,7 +27,7 @@ const page: FC<pageProps> = ({}) => {
         <div>
           <section className='search-box'>
             <p className='text-[#F6F8E2] font-light opacity-50 mt-10 mr-10'>What product are you looking for?</p>
-            <input className='placeholder-white placeholder-opacity-50 w-full mt-3  rounded-full bg-[#3C3D42] outline-none p-2 pl-5' type="text" placeholder='search...'/>
+            <input className='placeholder-[#3C3D42] placeholder-opacity-50 w-full mt-3 rounded-sm bg-[#E0DDCA] text-[#3C3D42] outline-none p-2 pl-5' type="text" placeholder='search...'/>
           </section>
           <section className='category'>
             <p className='text-[#F6F8E2] mt-10 text-xl'>category</p>
@@ -58,11 +58,11 @@ const page: FC<pageProps> = ({}) => {
             <div className='mt-10'>
               <p className='text-[#F6F8E2] text-xl'>last search</p>
               <ul className='history-list mt-3'>
-                <li className='history-item mb-4 p-5 text-[#3C3D42]'>item1</li>
-                <li className='history-item mb-4 p-5 text-[#3C3D42]'>item2</li>
-                <li className='history-item mb-4 p-5 text-[#3C3D42]'>item3</li>
-                <li className='history-item mb-4 p-5 text-[#3C3D42]'>item3</li>
-                <li className='history-item mb-4 p-5 text-[#3C3D42]'>item3</li>
+                <li className='history-item rounded-sm mb-4 p-5 text-[#3C3D42]'>item1</li>
+                <li className='history-item rounded-sm mb-4 p-5 text-[#3C3D42]'>item2</li>
+                <li className='history-item rounded-sm mb-4 p-5 text-[#3C3D42]'>item3</li>
+                <li className='history-item rounded-sm mb-4 p-5 text-[#3C3D42]'>item3</li>
+                <li className='history-item rounded-sm mb-4 p-5 text-[#3C3D42]'>item3</li>
               </ul>
             </div>
           </section>
