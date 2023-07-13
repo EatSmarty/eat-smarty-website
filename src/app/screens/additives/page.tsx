ad94d498eeb5744703a4755029885d5e0ef4351e
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-13 19:46:45 +0330

    add content to page addtives

--- src/app/screens/additives/page.tsx
+++ src/app/screens/additives/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Home from '@/app/page'
 import { FC } from 'react'
 
@@ -7,8 +9,42 @@ interface pageProps {
 
 const page: FC<pageProps> = ({}) => {
   return (
-    <div>
-        <Home></Home>
+    <div className='additives-screen'>
+      <Home></Home>
+        <header className='flex justify-center items-center mt-7'>
+          <section className='search-box'>
+            <p className='text-[#F6F8E2] mt-10 mr-10'>Additives</p>
+            <input className='placeholder-[#3C3D42] placeholder-opacity-50 w-full mt-3 rounded-sm bg-[#E0DDCA] text-[#3C3D42] outline-none p-2 px-12 pl-5' type="text" placeholder='search...'/>
+          </section>
+        </header>
+        <main className='main-content'>
+          <section className='filter-box flex justify-center items-center mt-5 ml-10'>
+            <div>
+              <p className='title'>Colors</p>
+              <input type="checkbox" />
+            </div>
+            <div>
+              <p className='title'>Preservatives</p>
+              <input type="checkbox" />
+            </div>
+            <div>
+              <p className='title'>Anticidants</p>
+              <input type="checkbox" />
+            </div>
+            <div>
+              <p className='title'>Sweeteners</p>
+              <input type="checkbox" />
+            </div>
+            <div>
+              <p className='title'>Emulsitiers</p>
+              <input type="checkbox" />
+            </div>
+            <div>
+              <p className='title'>Others</p>
+              <input type="checkbox" />
+            </div>
+          </section>
+        </main>
     </div>
   )
 }
