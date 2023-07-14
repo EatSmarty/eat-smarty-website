253895b311bf5ba89ab1172847d1e4c09ce0b61c
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-14 20:04:32 +0330

    set margin to icon and title

--- src/app/screens/additives/page.tsx
+++ src/app/screens/additives/page.tsx
@@ -13,15 +13,13 @@ const page: FC<pageProps> = ({}) => {
       <Home></Home>
         <header className='flex justify-center items-center mt-7'>
           <section className='search-box flex justify-between items-center'>
-              <div className='flex justify-center items-center'>
-                <p className='text-[#F6F8E2]'>Additives</p>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <p className='text-[#F6F8E2] mr-40'>Additives</p>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ml-10">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
                 </svg>
-              </div>
           </section>
         </header>
-        <main className='main-content flex justify-center items-center mt-5'>
+        <main className='main-content flex justify-center items-center mt-4'>
           <section className='filter-box'>
             <div>
               <input className='placeholder-[#3C3D42] placeholder-opacity-50 w-full mt-3 rounded-sm bg-[#E0DDCA] text-[#3C3D42] outline-none p-2 px-12 pl-5' type="text" placeholder='search...'/>
