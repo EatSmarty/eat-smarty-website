6d04cc0e027cab171303fb4e063acb36d9d331e5
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-15 19:46:44 +0330

    set flex to scan page

--- src/app/screens/scan/page.tsx
+++ src/app/screens/scan/page.tsx
@@ -9,6 +9,9 @@ const page: FC<pageProps> = ({}) => {
   return (
     <div>
         <Home></Home>
+        <div className='container flex justify-center items-center min-h-screen'>
+          <a className='scan p-5 flex justify-center items-center'>tap to scan</a>
+        </div>
     </div>
   )
 }
