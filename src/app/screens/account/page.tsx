3e1b1ff6bebfe7dc5e82079353962b32296463d9
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-15 19:57:18 +0330

    create  structure account page

--- src/app/screens/account/page.tsx
+++ src/app/screens/account/page.tsx
@@ -7,8 +7,21 @@ interface pageProps {
 
 const page: FC<pageProps> = ({}) => {
   return (
-    <div>
+    <div className='account-screen'>
         <Home></Home>
+        <header className='header text-[#F6F8E2] flex justify-center items-center mt-7'>
+          <h4 className='Account'>Account</h4>
+        </header>
+        <main className='flex justify-start items-center mt-10'>
+          <section className='common'>
+            <p className='text-[#F6F8E2] ml-10'>common</p>
+            <div className='common-items'>
+              <div className='language'></div>
+              <div className='theme'></div>
+              <div className='notification'></div>
+            </div>
+          </section>
+        </main>
     </div>
   )
 }
