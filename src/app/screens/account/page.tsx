6ee88cf69f3df13a05324d70be18e88bec1927eb
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-16 20:56:25 +0330

    edit account component

--- src/app/screens/account/page.tsx
+++ src/app/screens/account/page.tsx
@@ -1,6 +1,8 @@
 import Home from '@/app/page'
 import { FC } from 'react'
 import Header from './header'
+import Common from './common'
+import Account from './account'
 
 interface pageProps {
   
@@ -12,32 +14,8 @@ const page: FC<pageProps> = ({}) => {
         <Home></Home>
         <Header></Header>
         <main className='main-content flex justify-center items-center mt-10 flex-col'>
-          
-          {/* account */}
-          <section className='account w-full mt-10'>
-            <p className='text-[#F6F8E2]'>Account</p>
-            <div className='account-items text-[#F6F8E2]'>
-              <div className='email flex justify-between itmes-center bg-[#E0DDCA] text-[#3C3D42] px-12 p-2 mt-3 rounded-sm pl-5 pr-5'>
-                <div className='flex justify-center items-center'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-                </svg>
-                  <p>Email</p>
-                </div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-8 mr-2">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                </svg>
-              </div>
-              <div className='theme flex justify-between itmes-center bg-[#E0DDCA] text-[#3C3D42] px-12 p-2 mt-3 rounded-sm pl-5 pr-5'>
-                <div className='flex justify-center items-center'>
-                  <a href='#' className='text-red-500'>Logout</a>
-                </div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-8 mr-2">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                </svg>
-              </div>
-            </div>
-          </section>
+          <Common></Common>
+          <Account></Account>
           {/* about */}
           <section className='about w-full mt-10'>
             <p className='text-[#F6F8E2]'>About</p>
