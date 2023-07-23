864d193dcf3ab2c89a78a7659e7507f3135bd37e
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-23 20:49:48 +0330

    change src

--- src/app/screens/home/header.tsx
+++ src/app/screens/home/header.tsx
@@ -9,7 +9,7 @@ const Header: FC<HeaderProps> = ({}) => {
     <header className='home-header flex justify-center items-center mt-7'>
         <section className='flex justify-between items-center '>
           <div className='flex justify-center items-center mr-20'>
-            <img className='w-10 rounded-full mr-3' src='../../../../public/avatar-2023.jpg' alt="avatar" />
+            <img className='w-10 rounded-full mr-3' src='/avatar-2023.jpg' alt="avatar" />
             <h4 className='text-[#F6F8E2] mr-1'>Hey Hosein</h4>
             <div className='wave text-xl'>👋</div>
           </div>
