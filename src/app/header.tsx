d3b154afac192a748002342134758170431360cf
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-25 23:42:42 +0330

    update search box section

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 const Header: FC<HeaderProps> = ({}) => {
   return <header className='home-header flex justify-between items-center w-full fixed'>
             <div className='header-l flex justify-center items-center mt-5'>
-              <h4 className='text-3xl font-bold text-[#0069FE]'>Home</h4>
+              <h4 className='text-3xl font-bold text-[#0069FE] opacity-80'>Home</h4>
             </div>
             <a href='https://halalvar.com/' className="header-r bg-[#0069FE] text-[#fff] rounded-full p-3 mt-5 flex justify-center items-center">
               <p className='mr-1'>halalvar</p>
