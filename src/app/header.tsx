558513ef34e2a25c274e55bbfb6981bf831372fc
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-25 23:16:37 +0330

    update header style

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -9,9 +9,12 @@ const Header: FC<HeaderProps> = ({}) => {
             <div className='header-l flex justify-center items-center mt-5'>
               <h4 className='text-3xl font-bold text-[#0069FE]'>Home</h4>
             </div>
-              <a href='https://halalvar.com/' className="header-r bg-[#0069FE] text-[#fff] rounded-full p-3 mt-5">
-                Hal.
-              </a>
+            <a href='https://halalvar.com/' className="header-r bg-[#0069FE] text-[#fff] rounded-full p-3 mt-5 flex justify-center items-center">
+              <p className='mr-1'>halalvar</p>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+              </svg>
+            </a>
   </header>
 }
 
