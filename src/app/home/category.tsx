a230c026432a6b5d5efe86e08e220feb0a47330f
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-26 18:38:26 +0330

    set style to category

--- src/app/home/category.tsx
+++ src/app/home/category.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { FC } from 'react'
 
 interface categoryProps {
@@ -7,28 +8,46 @@ interface categoryProps {
 const Category: FC<categoryProps> = ({}) => {
   return <div>
     <section className='category'>
-            <p className='text-[#F6F8E2] mt-10 text-xl'>category</p>
-            <div className='containers flex w-100 scroll-section-outer'>
-                <div className='scroll-section-inner flex relative mt-5'>
-                  <div className='item1 w-16 mr-10'>
-                    <img src="/fruit.png" alt="" />
-                  </div>
-                  <div className='item2 w-16 mr-10'>
-                    <img src="/trick-or-treat.png" alt="" />
-                  </div>
-                  <div className='item3 w-16 mr-10'>
-                    <img src="/frozen-food.png" alt="" />
-                  </div>
-                  <div className='item4 w-16 mr-10'>
-                    <img src="/soft-drink.png" alt="" />
-                  </div>
-                  <div className='item5 w-16 mr-10'>
-                    <img src="/bread.png" alt="" />
-                  </div>
-                  <div className='item6 w-16 mr-10'>
-                    <img src="/begetable.png" alt="" />
-                  </div>
-                </div>
+            <p className='mt-10 text-xl'>category</p>
+            <div className='category-items'>
+                    <Image src="/fruit-3d.jpg"
+                      width={100}
+                      height={100}
+                      alt="fruite"
+                      className='rounded'
+                     />
+                  <Image src="/drink-3d.jpg"
+                      width={100}
+                      height={100}
+                      alt="fruite"
+                      className='rounded'
+                     />
+                  <Image src="/vegetable-3d.jpg"
+                      width={100}
+                      height={100}
+                      alt="fruite"
+                      className='rounded'
+                     /><Image src="/vegetable-3d.jpg"
+                     width={100}
+                     height={100}
+                     alt="fruite"
+                     className='rounded'
+                    /><Image src="/vegetable-3d.jpg"
+                    width={100}
+                    height={100}
+                    alt="fruite"
+                    className='rounded'
+                   /><Image src="/vegetable-3d.jpg"
+                   width={100}
+                   height={100}
+                   alt="fruite"
+                   className='rounded'
+                  /><Image src="/vegetable-3d.jpg"
+                  width={100}
+                  height={100}
+                  alt="fruite"
+                  className='rounded'
+                 />
             </div>
         </section>
   </div>
