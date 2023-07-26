25822d4704c02d2a5ab1fdcd3d5907116b8352e7
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-26 19:37:26 +0330

    create advertise  component

---
+++ src/app/home/advertise.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+interface advertiseProps {
+  
+}
+
+const Advertise: FC<advertiseProps> = ({}) => {
+  return <div className='advertise flex justify-center items-center mt-10 rounded'>
+    <p className='text-2xl p-10'>Advertise</p>
+  </div>
+}
+
+export default Advertise
