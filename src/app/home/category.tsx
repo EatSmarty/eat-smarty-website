7a42265b2fcdc1c1bf460c3c3b3db8428842fdee
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-26 19:08:32 +0330

    set style to category(overlay text)

--- src/app/home/category.tsx
+++ src/app/home/category.tsx
@@ -8,46 +8,41 @@ interface categoryProps {
 const Category: FC<categoryProps> = ({}) => {
   return <div>
     <section className='category'>
-            <p className='mt-10 text-xl'>category</p>
+            <p className='mt-10 text-2xl'>category</p>
             <div className='category-items'>
+                  <div className="container">
                     <Image src="/fruit-3d.jpg"
-                      width={100}
-                      height={100}
-                      alt="fruite"
-                      className='rounded'
-                     />
-                  <Image src="/drink-3d.jpg"
-                      width={100}
-                      height={100}
-                      alt="fruite"
-                      className='rounded'
-                     />
-                  <Image src="/vegetable-3d.jpg"
-                      width={100}
-                      height={100}
-                      alt="fruite"
-                      className='rounded'
-                     /><Image src="/vegetable-3d.jpg"
-                     width={100}
-                     height={100}
-                     alt="fruite"
-                     className='rounded'
-                    /><Image src="/vegetable-3d.jpg"
-                    width={100}
-                    height={100}
-                    alt="fruite"
-                    className='rounded'
-                   /><Image src="/vegetable-3d.jpg"
-                   width={100}
-                   height={100}
-                   alt="fruite"
-                   className='rounded'
-                  /><Image src="/vegetable-3d.jpg"
-                  width={100}
-                  height={100}
-                  alt="fruite"
-                  className='rounded'
-                 />
+                        width={100}
+                        height={100}
+                        alt="fruite"
+                        className='image rounded'
+                      />
+                      <div className="overlay">
+                        <div className='text'>Fruit</div>
+                      </div>
+                  </div>
+                  <div className="container">
+                    <Image src="/drink-3d.jpg"
+                        width={100}
+                        height={100}
+                        alt="fruite"
+                        className='image rounded'
+                      />
+                      <div className="overlay">
+                        <div className='text'>Drink</div>
+                      </div>
+                  </div>
+                  <div className="container">
+                    <Image src="/vegetable-3d.jpg"
+                        width={100}
+                        height={100}
+                        alt="fruite"
+                        className='image rounded'
+                      />
+                      <div className="overlay">
+                        <div className='text'>Vegetabls</div>
+                      </div>
+                  </div>
             </div>
         </section>
   </div>
