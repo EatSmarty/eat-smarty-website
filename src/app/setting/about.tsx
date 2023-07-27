d4b3188ccacd9cbbf68ff4d9058039921671cd85
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-27 23:19:40 +0330

    update  setting items style

--- src/app/setting/about.tsx
+++ src/app/setting/about.tsx
@@ -6,26 +6,26 @@ interface aboutProps {
 
 const About: FC<aboutProps> = ({}) => {
   return <div>
-    <section className='common mt-40 text-2xl'>
-            <p className=''>About</p>
-            <div className='common-items'>
-              <div className='language flex justify-between itmes-center w-full text-[#3C3D42] p-2 mt-3 rounded cursor-pointer'>
+    <section className='common mt-20 text-2xl'>
+          <p className=''>About</p>
+          <div className='common-items text-xl'>
+              <div className='setting-item language flex justify-between itmes-center w-full py-3 mt-3 rounded cursor-pointer'>
                 <div className='flex justify-center items-center'>
-                  <p>FAQ</p>
+                  <p className='ml-4'>FAQ</p>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-8 mr-2 hover:mr-1 duration-200 font-bold">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-8 mr-4 hover:mr-3 duration-200 font-bold">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                 </svg>
               </div>
-            </div>
-            <div className='language flex justify-between itmes-center w-full text-[#3C3D42] p-2 mt-3 rounded cursor-pointer'>
+            <div className='setting-item contact-us flex justify-between itmes-center w-full py-3 mt-3 rounded cursor-pointer'>
                 <div className='flex justify-center items-center'>
-                  <p>Contact Us</p>
+                  <p className='ml-4'>Contact Us</p>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-8 mr-2 hover:mr-1 duration-200 font-bold">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-8 mr-4 hover:mr-3 duration-200 font-bold">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                 </svg>
               </div>
+          </div>
           </section>
   </div>
 }
--- src/app/setting/common.tsx
+++ src/app/setting/common.tsx
@@ -8,26 +8,26 @@ const Common: FC<commonProps> = ({}) => {
   return <div>
     <section className='common mt-40 text-2xl'>
             <p className=''>Common</p>
-            <div className='common-items'>
-              <div className='language flex justify-between itmes-center w-full text-[#3C3D42] p-2 mt-3 rounded'>
+            <div className='common-items text-xl'>
+              <div className='setting-item language flex justify-between itmes-center w-full text-[#3C3D42] py-2 mt-3 rounded cursor-pointer'>
                 <div className='flex justify-center items-center'>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ml-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
                   </svg>
                   <p>Language</p>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-8 mr-2 hover:mr-1 duration-200 font-bold">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-8 mr-4 hover:mr-3 duration-200 font-bold">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                 </svg>
               </div>
-              <div className='theme flex justify-between itmes-center text-[#3C3D42] p-2 mt-3 rounded'>
+              <div className='setting-item theme flex justify-between itmes-center text-[#3C3D42] py-2 mt-3 rounded'>
                 <div className='flex justify-center items-center'>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ml-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
                   </svg>
                   <p>Theme</p>
                 </div>
-                <label className="switch">
+                <label className="switch mr-4">
                     <input type="checkbox"/>
                     <span className="slider round"></span>
                 </label>
