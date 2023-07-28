c175dbd86e5d0573cddf4e9748b2b44e6cc28dd7
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-28 20:05:37 +0330

    update faq page

--- src/app/setting/faq/faq.tsx
+++ src/app/setting/faq/faq.tsx
@@ -7,7 +7,7 @@ interface faqProps {
 const Faq: FC<faqProps> = ({}) => {
   return <div>
   <section className='contactus mt-20 text-2xl flex justefy-center items-center flex-col'>
-      <p className='title text-bold text-[#0069FE] mb-10'>FAQ</p>
+      <p className='title text-bold text-[#0069FE] mb-10'>Contact Us</p>
       <div className='contact-box rounded flex justify-center items-center flex-col'>
           
       </div>
