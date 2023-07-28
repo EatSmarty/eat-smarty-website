1374ae6972670b2060ee946419f25afb2256fe06
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-28 18:46:27 +0330

    add about > faq link

--- src/app/setting/about.tsx
+++ src/app/setting/about.tsx
@@ -10,14 +10,14 @@ const About: FC<aboutProps> = ({}) => {
     <section className='common mt-20 text-2xl'>
           <p className=''>About</p>
           <div className='common-items text-xl'>
-              <div className='setting-item language flex justify-between itmes-center w-full py-3 mt-3 rounded cursor-pointer'>
-                <div className='flex justify-center items-center'>
-                  <p className='ml-4'>FAQ</p>
-                </div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-8 mr-4 hover:mr-3 duration-200 font-bold">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                </svg>
-              </div>
+            <Link href={'/setting/faq'} className='setting-item contact-us flex justify-between itmes-center w-full py-3 mt-3 rounded cursor-pointer'>
+                  <div className='flex justify-center items-center'>
+                    <p className='ml-4'>FAQ</p>
+                  </div>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-8 mr-4 hover:mr-3 duration-200 font-bold">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                  </svg>
+            </Link>
             <Link href={"/setting/contactus"}  className='setting-item contact-us flex justify-between itmes-center w-full py-3 mt-3 rounded cursor-pointer'>
                 <div className='flex justify-center items-center'>
                   <p className='ml-4'>Contact Us</p>
