c524bb7f5942c9ac1e72e6e1f38db55e037d3beb
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-28 12:50:41 +0330

    contactus page

---
+++ src/app/setting/contactus/contactus.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import Header from '../header'
+
+interface contactusProps {
+  
+}
+
+const Contactus: FC<contactusProps> = ({}) => {
+  return <div>
+    
+  </div>
+}
+
+export default Contactus
