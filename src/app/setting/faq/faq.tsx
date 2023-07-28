27768e95164a1cb3c99d03d8d54d33318fe12b35
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-28 18:46:55 +0330

    create faq page

---
+++ src/app/setting/faq/faq.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react'
+
+interface faqProps {
+  
+}
+
+const Faq: FC<faqProps> = ({}) => {
+  return <div>
+  <section className='contactus mt-20 text-2xl flex justefy-center items-center flex-col'>
+      <p className='title text-bold text-[#0069FE] mb-10'>FAQ</p>
+      <div className='contact-box rounded flex justify-center items-center flex-col'>
+          
+      </div>
+  </section>
+</div>
+}
+
+export default Faq
