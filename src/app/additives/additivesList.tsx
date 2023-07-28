800781a4fa6f1bc44b4ff906326ce6c207b25673
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-28 21:01:02 +0330

    set desc directory

--- src/app/additives/additivesList.tsx
+++ src/app/additives/additivesList.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import additivesJson from './additives.json'
+import Link from 'next/link'
 
 interface additivesListProps {
   
@@ -9,12 +10,14 @@ let colour = additivesJson.Colour
 const AdditivesList: FC<additivesListProps> = ({}) => {
 
   return <div className='e-food mx-10'>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[0].enumber}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-orange-400 text-[#fff] p-1 rounded'>{colour[0].badge}</div>
+            <Link href={"additives/desc"}>
+              <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
+                <p className='title'>{colour[0].enumber}</p>
+                <div className='flex justify-center items-center'>
+                  <div className='color-palete bg-orange-400 text-[#fff] p-1 rounded'>{colour[0].badge}</div>
+                </div>
               </div>
-            </div>
+            </Link>
             {/* <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
               <p className='title'>{colour[1]['e-number']}</p>
               <div className='flex justify-center items-center'>
