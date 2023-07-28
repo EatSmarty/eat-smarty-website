f62eee87e228bb3f8e4498e0076b0162ced1cb19
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-28 20:29:18 +0330

    update additives list page

--- src/app/additives/additivesList.tsx
+++ src/app/additives/additivesList.tsx
@@ -5,18 +5,17 @@ interface additivesListProps {
   
 }
 let colour = additivesJson.Colour
-console.log(colour[2]['e-number'])
-const AdditivesList: FC<additivesListProps> = ({}) => {
 
+const AdditivesList: FC<additivesListProps> = ({}) => {
 
   return <div className='e-food mx-10'>
             <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[0]['e-number']}</p>
+              <p className='title'>{colour[0].enumber}</p>
               <div className='flex justify-center items-center'>
-                <div className='color-palete bg-orange-400 text-[#fff] p-1 rounded'>{colour[0]['badge']}</div>
+                <div className='color-palete bg-orange-400 text-[#fff] p-1 rounded'>{colour[0].badge}</div>
               </div>
             </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
+            {/* <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
               <p className='title'>{colour[1]['e-number']}</p>
               <div className='flex justify-center items-center'>
                 <div className='color-palete bg-green-400 text-[#fff] p-1 rounded'>{colour[1]['badge']}</div>
@@ -27,104 +26,13 @@ const AdditivesList: FC<additivesListProps> = ({}) => {
               <div className='flex justify-center items-center'>
                 <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>{colour[2]['badge']}</div>
               </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[3]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>{colour[4]['badge']}</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[4]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[5]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[6]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[7]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[8]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[9]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[10]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[11]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[12]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[13]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[14]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[15]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[16]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[17]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[18]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>HARAM</div>
-              </div>
-            </div>
+            </div> */}
   </div>
 }
 
 export default AdditivesList
+
+
+// {colour.map((index) => (
+//   index.enumber
+// ))}
