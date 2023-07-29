b88d23300c933b03dfd23f31fbf5fadfa7789e45
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-29 18:55:11 +0330

    create scan header

---
+++ src/app/scan/header.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface HeaderProps {
+  
+}
+
+const Header: FC<HeaderProps> = ({}) => {
+  return <header className='home-header flex justify-between items-center w-full fixed'>
+            <div className='header-l flex justify-center items-center mt-5'>
+              <h4 className='text-3xl font-bold text-[#0069FE] opacity-80'>Scan</h4>
+            </div>
+  </header>
+}
+
+export default Header
