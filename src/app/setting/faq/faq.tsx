3f12f3ec23bc282adcc17a584800e876b12653af
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-30 01:33:22 +0330

    sample data

--- src/app/setting/faq/faq.tsx
+++ src/app/setting/faq/faq.tsx
@@ -1,15 +1,33 @@
 import { FC } from 'react'
+import Accordion from './accordion'
 
 interface faqProps {
   
 }
 
 const Faq: FC<faqProps> = ({}) => {
+  const accordionData = [
+    {
+        title: "test1?",
+        desc: "",
+    },
+    {
+        title: "test2?",
+        desc: "",
+    },
+    {
+        title: "test3?",
+        desc: "",
+    }
+]
   return <div>
   <section className='contactus mt-20 text-2xl flex justefy-center items-center flex-col'>
       <p className='title text-bold text-[#0069FE] mb-10'>FAQ</p>
       <div className='contact-box rounded flex justify-center items-center flex-col'>
-          
+          {accordionData.map((data, index) => {
+            // eslint-disable-next-line react/jsx-key
+            return <Accordion></Accordion>
+          })}
       </div>
   </section>
 </div>
