2602007fa6e557fdbcbe4bc244733cb4dc6a4c0c
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-30 01:59:27 +0330

    update accordion

--- src/app/setting/faq/accordion.tsx
+++ src/app/setting/faq/accordion.tsx
@@ -1,11 +1,25 @@
-import { FC } from 'react'
+import { FC, MouseEventHandler, ReactNode } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 
 interface accordionProps {
-  
+  open: ReactNode
+  toggle: MouseEventHandler
+  title: ReactNode
+  desc: ReactNode
 }
 
-const Accordion: FC<accordionProps> = ({}) => {
-  return <div>accordion</div>
+const Accordion: FC<accordionProps> = ({open, toggle, title, desc}) => {
+  return <div className='p-10'>
+    <div className='bg-red-200' onClick={toggle}>
+        <p>{title}</p>
+        <div className=''>
+            {open ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </div>
+    </div>
+    <Collapse isOpene={open}>
+        <div className='desc'>{desc}</div>
+    </Collapse>
+  </div>
 }
 
 export default Accordion
