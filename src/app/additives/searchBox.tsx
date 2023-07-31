b17e0a573e63b256b6e6c9a3a413c3117fbc321b
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-31 19:21:50 +0330

    change margin

--- src/app/additives/searchBox.tsx
+++ src/app/additives/searchBox.tsx
@@ -5,7 +5,7 @@ interface searchBoxProps {
 }
 
 const SearchBox: FC<searchBoxProps> = ({}) => {
-  return <section className='search-box mt-40 text-2xl'>
+  return <section className='search-box my-20 text-2xl'>
   <div className='search-input flex items-center justify-center rounded'>
     <input className='placeholder-[#3C3D42] placeholder-opacity-50 w-full mt-3 rounded-sm outline-none px-2 flex' type="text" placeholder='search...'/>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-gray-500 cursor-pointer absolute">
