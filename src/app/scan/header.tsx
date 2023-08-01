26ac70367eb74e1c64dbafd453a098f990cd6390
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-01 20:55:21 +0330

    change justify style

--- src/app/scan/header.tsx
+++ src/app/scan/header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
 }
 
 const Header: FC<HeaderProps> = ({}) => {
-  return <header className='home-header flex justify-between items-center w-full fixed'>
+  return <header className='home-header flex justify-around items-center w-full fixed'>
             <div className='header-l flex justify-center items-center mt-5'>
               <h4 className='text-3xl font-bold text-[#0069FE] opacity-80'>Scan</h4>
             </div>
