abfcdac105044ea28c640104e0eca51f69958aa7
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-01 20:55:40 +0330

    import FAQ question

--- src/app/setting/faq/faq.tsx
+++ src/app/setting/faq/faq.tsx
@@ -3,16 +3,36 @@
 function Faq() {
     const data = [
         {
-            title: "test1?",
-            desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt laborum vel corrupti neque consequuntur reprehenderit ut, porro omnis quidem fugit officiis, eaque enim tempora laboriosam praesentium? Provident pariatur ducimus repellat.",
+            title: "What is your startup's main service?",
+            desc: "We specialize in providing a convenient and efficient solution for identifying halal food products using their barcodes. With our technology, you can quickly determine whether a product meets your halal dietary requirements.",
         },
         {
-            title: "test2?",
-            desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt laborum vel corrupti neque consequuntur reprehenderit ut, porro omnis quidem fugit officiis, eaque enim tempora laboriosam praesentium? Provident pariatur ducimus repellat.",
+            title: "How does your barcode scanning process work?",
+            desc: "Our cutting-edge barcode scanning technology employs advanced algorithms to analyze the barcode data of food products. It cross-references this data with our extensive halal food database to determine the product's halal status accurately.",
         },
         {
-            title: "test3?",
-            desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt laborum vel corrupti neque consequuntur reprehenderit ut, porro omnis quidem fugit officiis, eaque enim tempora laboriosam praesentium? Provident pariatur ducimus repellat.",
+            title: "Is your halal identification process reliable?",
+            desc: "Absolutely! Our startup has invested significant efforts in building a robust and up-to-date halal food database. We continuously verify and update the information to ensure the highest level of accuracy in identifying halal products.",
+        },
+        {
+            title: "Can your service be used internationally?",
+            desc: "Yes, our service is designed to work internationally. As long as the product's barcode can be scanned, our system can determine its halal status, regardless of the country of origin or purchase.",
+        },
+        {
+            title: "What languages does your app support?",
+            desc: "Currently, our app supports multiple languages, making it accessible to a broader audience. English is the default language, but we offer options for users to choose from various other languages to enhance user experience and understanding.",
+        },
+        {
+            title: "Is your startup affiliated with any halal certification organizations?",
+            desc: "While we strive to collaborate with reputable halal certification organizations to enrich our database, we operate as an independent entity. Our identification process is based on our own comprehensive research and verification.",
+        },
+        {
+            title: "Can users contribute to the database?",
+            desc: "Yes, we value user contributions and encourage our community to provide feedback and suggest new products or updates to existing entries. Users can help us ensure the database remains accurate and relevant.",
+        },
+        {
+            title: "What measures do you take to ensure data privacy and security?",
+            desc: "Protecting our users' data is a top priority. We employ industry-standard security protocols to safeguard all user information and follow strict privacy policies to maintain confidentiality.",
         }
       ]
   return <div>
