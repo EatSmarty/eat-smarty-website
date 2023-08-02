e959a3d3f3dac461a61f7d985ec553ac6726abf7
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-02 20:18:49 +0330

    create why sec

---
+++ src/app/home/why.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react'
+import Image from 'next/image'
+
+interface whyProps {
+  
+}
+
+const Why: FC<whyProps> = ({}) => {
+  return <div className='why-section flex justify-between items-center my-40'>
+            <div className='w-1/2'>
+                <h1 className='text-5xl mb-4 text-gray-500'>Why Halalvar?</h1>
+                <p>
+                    Halal & Healthy provides information about ingredients and the origin of food additives (E-Code) by a simple scan. 
+                    If a product is suspicious, the user will get an alternative list of Halal products.
+                </p>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="why-s-icon w-20 h-20 text-[#0069FE]">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+            </svg>
+  </div>
+}
+
+export default Why
