a7e3aaa767838fe6a799cd43ead387f1718ab068
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-05 14:04:22 +0330

    set clsx style

--- src/app/home/category.tsx
+++ src/app/home/category.tsx
@@ -1,94 +1,148 @@
+import clsx from 'clsx'
 import Image from 'next/image'
 import { FC } from 'react'
 
 interface categoryProps {
-  
+
 }
 
-const Category: FC<categoryProps> = ({}) => {
+const Category: FC<categoryProps> = ({ }) => {
   return <div>
-    <section className='category mt-20 mx-5'>
-            {/* <p className='mt-10 text-2xl'>category</p> */}
-            <div className='category-items'>
-                  <div className="container cursor-pointer">
-                    <Image src="https://halalvar.github.io/halalvar/fruit-3d.jpg"
-                        width={100}
-                        height={100}
-                        alt="fruite"
-                        className='image rounded'
-                      />
-                      <div className="overlay">
-                        <div className='text'>Fruit</div>
-                      </div>
-                  </div>
-                  <div className="container cursor-pointer">
-                    <Image src="https://halalvar.github.io/halalvar/drink-3d.jpg"
-                        width={100}
-                        height={100}
-                        alt="drink"
-                        className='image rounded'
-                      />
-                      <div className="overlay">
-                        <div className='text'>Drink</div>
-                      </div>
-                  </div>
-                  <div className="container cursor-pointer">
-                    <Image src="https://halalvar.github.io/halalvar/vegetable-3d.jpg"
-                        width={100}
-                        height={100}
-                        alt="vegetabls"
-                        className='image rounded'
-                      />
-                      <div className="overlay">
-                        <div className='text'>Vegetabls</div>
-                      </div>
-                  </div>
-                  <div className="container cursor-pointer">
-                    <Image src="https://halalvar.github.io/halalvar/bread-3d.jpg"
-                        width={100}
-                        height={100}
-                        alt="bread"
-                        className='image rounded'
-                      />
-                      <div className="overlay">
-                        <div className='text'>Bread</div>
-                      </div>
-                  </div>
-                  <div className="container cursor-pointer">
-                    <Image src="https://halalvar.github.io/halalvar/fresh-food-3d.jpg"
-                        width={100}
-                        height={100}
-                        alt="fresh food"
-                        className='image rounded'
-                      />
-                      <div className="overlay">
-                        <div className='text'>FreshFood</div>
-                      </div>
-                  </div>
-                  <div className="container cursor-pointer">
-                    <Image src="https://halalvar.github.io/halalvar/ice-cream-3d.jpg"
-                        width={100}
-                        height={100}
-                        alt="ice cream"
-                        className='image rounded'
-                      />
-                      <div className="overlay">
-                        <div className='text'>Ice-cream</div>
-                      </div>
-                  </div>
-                  <div className="container cursor-pointer">
-                    <Image src="https://halalvar.github.io/halalvar/ice-cream-3d.jpg"
-                        width={100}
-                        height={100}
-                        alt="ice cream"
-                        className='image rounded'
-                      />
-                      <div className="overlay">
-                        <div className='text'>Ice-cream</div>
-                      </div>
-                  </div>
-            </div>
-        </section>
+    <section className={clsx(
+      "category",
+      "mt-14 mx-5"
+    )}>
+      <div className={clsx(
+        "category-items"
+      )}>
+        <div className={clsx(
+          "container",
+          "cursor-pointer"
+        )}>
+          <Image src="https://halalvar.github.io/halalvar/fruit-3d.jpg"
+            width={100}
+            height={100}
+            alt="fruite"
+            className={clsx(
+              "image",
+              "rounded"
+            )}
+          />
+          <div className={clsx(
+            "overlay"
+          )}>
+            <div className={clsx(
+              "text"
+            )}>Fruit</div>
+          </div>
+        </div>
+        <div className={clsx(
+          "container",
+          "cursor-pointer"
+        )}>
+          <Image src="https://halalvar.github.io/halalvar/drink-3d.jpg"
+            width={100}
+            height={100}
+            alt="drink"
+            className={clsx(
+              "image",
+              "rounded"
+            )}
+          />
+          <div className={clsx(
+            "overlay"
+          )}>
+            <div className={clsx(
+              "text"
+            )}>Drink</div>
+          </div>
+        </div>
+        <div className={clsx(
+          "container",
+          "cursor-pointer"
+        )}>
+          <Image src="https://halalvar.github.io/halalvar/vegetable-3d.jpg"
+            width={100}
+            height={100}
+            alt="vegetabls"
+            className={clsx(
+              "image",
+              "rounded"
+            )}
+          />
+          <div className={clsx(
+            "overlay"
+          )}>
+            <div className={clsx(
+              "text"
+            )}>Vegetabls</div>
+          </div>
+        </div>
+        <div className={clsx(
+          "container",
+          "cursor-pointer"
+        )}>
+          <Image src="https://halalvar.github.io/halalvar/bread-3d.jpg"
+            width={100}
+            height={100}
+            alt="bread"
+            className={clsx(
+              "image",
+              "rounded"
+            )}
+          />
+          <div className={clsx(
+            "overlay"
+          )}>
+            <div className={clsx(
+              "text"
+            )}>Bread</div>
+          </div>
+        </div>
+        <div className={clsx(
+          "container",
+          "cursor-pointer"
+        )}>
+          <Image src="https://halalvar.github.io/halalvar/fresh-food-3d.jpg"
+            width={100}
+            height={100}
+            alt="fresh food"
+            className={clsx(
+              "image",
+              "rounded"
+            )}
+          />
+          <div className={clsx(
+            "overlay"
+          )}>
+            <div className={clsx(
+              "text"
+            )}>FreshFood</div>
+          </div>
+        </div>
+        <div className={clsx(
+          "container",
+          "cursor-pointer"
+        )}>
+          <Image src="https://halalvar.github.io/halalvar/ice-cream-3d.jpg"
+            width={100}
+            height={100}
+            alt="ice cream"
+            className={clsx(
+              "image",
+              "rounded"
+            )}
+          />
+          <div className={clsx(
+            "overlay"
+          )}>
+            <div className={clsx(
+              "text"
+            )}>Ice-cream</div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 }
 
