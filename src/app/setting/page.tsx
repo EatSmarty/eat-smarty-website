615d7389358fa9e3d366697b0ea42d31901f4fc4
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-05 11:42:10 +0330

    set title to setting page

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -2,13 +2,13 @@
 
 import Navigation from '@/app/page'
 import { FC } from 'react'
-import Header from './header'
+import Header from '../header'
 import Common from './common'
 import Search from '../home/search'
 import About from './about'
 
 interface pageProps {
-  
+
 }
 
 
@@ -16,7 +16,7 @@ const Page: FC<pageProps> = () => {
   return (
     <div className='home-screen mb-20'>
       <Navigation></Navigation>
-      <Header></Header>
+      <Header title="Setting"></Header>
       <main className='flex justify-center items-center'>
         <div>
           <Common></Common>
