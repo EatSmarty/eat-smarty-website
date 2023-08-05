f22182ce339b13d7d5b6451045bfdcd631b7e324
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-05 11:18:26 +0330

    clean header file

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,23 +1,34 @@
 import Image from 'next/image'
-import { FC } from 'react'
+import clsx from 'clsx';
+import Link from 'next/link';
 
-interface HeaderProps {
-  
+
+interface Props {
+  title: string | undefined | null;
+  // backLink?: string;
+  // halalStatus: "halal" | "mushbooh" | "haram"
 }
 
-const Header: FC<HeaderProps> = ({}) => {
-  return <header className='home-header flex justify-around items-center w-full fixed pt-5'>
-            <div className='header-l flex justify-center items-center'>
-              <h4 className='text-3xl font-bold text-[#0069FE] opacity-80'>Home</h4>
-            </div>
-            <Image
-              src="https://github.com/Halalvar/halalvar/blob/main/public/logo.png?raw=true"
-              width={70}
-              height={70}
-              alt='logo'
-              className='logo'
-            />
-  </header>
-}
 
-export default Header
+export default function Home({ title }: Props) {
+
+  return (
+    <header className={clsx(
+      "flex justify-around items-center",
+      "w-full",
+      "fixed",
+      "pt-5",
+    )}>
+      <div className='header-l flex justify-center items-center'>
+        <h4 className='text-3xl font-bold text-[#0069FE] opacity-80'>{title}</h4>
+      </div>
+      <Image
+        src="https://github.com/Halalvar/halalvar/blob/main/public/logo.png?raw=true"
+        width={70}
+        height={70}
+        alt='logo'
+        className='logo'
+      />
+    </header>
+  )
+}
