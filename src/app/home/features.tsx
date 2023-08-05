77e4db11bfc312ba645e271131b412e5b4529900
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-05 13:48:24 +0330

    update clx

--- src/app/home/features.tsx
+++ src/app/home/features.tsx
@@ -96,10 +96,23 @@ const Features: FC<featuresProps> = ({ }) => {
             "feature",
             "flex flex-col justfy-center items-center"
         )}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-10 h-10 duration-200 hover:text-[#0069FE] text-gray-500">
-                <path d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 24 24" stroke="currentColor"
+                className={clsx(
+                    "w-10 h-10",
+                    "duration-200 hover:text-[#0069FE]",
+                    "text-gray-500"
+                )}>
+                <path d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708
+                2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0
+                0118 0zm-9-3.75h.008v.008H12V8.25z" />
             </svg>
-            <p className='desc-feature font-bold text-gray-500 w-44 mt-5 text-center'>Comprehensive Information</p>
+            <p className={clsx(
+                "desc-feature",
+                "font-bold text-gray-500",
+                "w-44",
+                "mt-5 text-center"
+            )}>Comprehensive Information</p>
         </div>
     </section>
 }
