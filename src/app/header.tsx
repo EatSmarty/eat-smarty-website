1ae75a2471543cb9d89ab7db6c0c1f140d7f3214
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-05 11:41:19 +0330

    set clsx style

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import clsx from 'clsx';
-import Link from 'next/link';
 
 
 interface Props {
@@ -18,16 +17,20 @@ export default function Home({ title }: Props) {
       "w-full",
       "fixed",
       "pt-5",
+      "bg-[#fff]",
+      "border-b"
     )}>
       <div className='header-l flex justify-center items-center'>
-        <h4 className='text-3xl font-bold text-[#0069FE] opacity-80'>{title}</h4>
+        <h1 className='text-3xl font-bold text-[#0069FE] opacity-80'>{title}</h1>
       </div>
       <Image
         src="https://github.com/Halalvar/halalvar/blob/main/public/logo.png?raw=true"
         width={70}
         height={70}
         alt='logo'
-        className='logo'
+        className={clsx(
+          'logo',
+        )}
       />
     </header>
   )
