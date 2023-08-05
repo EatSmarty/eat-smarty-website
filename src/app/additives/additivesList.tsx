0e66141f26d362ea08473adfbecb94e8f7a23a20
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-05 11:16:43 +0330

    update header component

--- src/app/additives/additivesList.tsx
+++ src/app/additives/additivesList.tsx
@@ -1,44 +1,47 @@
-import { FC } from 'react'
-import additivesJson from './additives.json'
-import Link from 'next/link'
-
-interface additivesListProps {
-  
-}
-let colour = additivesJson.Colour
-
-const AdditivesList: FC<additivesListProps> = ({}) => {
-
-  return <div className='e-food mt-20'>
-            <Link href={"additives/desc"}>
-              <div className='item mt-3 rounded flex flex-col justify-around items-center w-28 h-28'>
-                <p className='title'>{colour[0].enumber}</p>
-                <div className='flex justify-center items-center'>
-                  <div className='color-palete w-24 text-center halal text-[#fff] p-1 rounded'>{colour[0].badge}</div>
-                </div>
-              </div>
-            </Link>
-            
+import Image from 'next/image'
+import clsx from 'clsx';
+import Link from 'next/link';
 
 
-            {/* <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[1]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-green-400 text-[#fff] p-1 rounded'>{colour[1]['badge']}</div>
-              </div>
-            </div>
-            <div className='item px-12 p-2 mt-3 rounded pl-5 pr-5 flex flex-col justify-between items-center w-32 h-32'>
-              <p className='title'>{colour[2]['e-number']}</p>
-              <div className='flex justify-center items-center'>
-                <div className='color-palete bg-red-400 text-[#fff] p-1 rounded'>{colour[2]['badge']}</div>
-              </div>
-            </div> */}
-  </div>
+// interface Props {
+//   title: string | undefined | null;
+// }
+
+
+export default function Home({ }) {
+
+  return (
+    <div className='e-food mt-20'>
+    </div>
+  )
 }
 
-export default AdditivesList
 
 
-// {colour.map((index) => (
-//   index.enumber
-// ))}
+
+
+
+
+
+
+// interface additivesListProps {
+
+// }
+
+// const AdditivesList: FC<additivesListProps> = ({ }) => {
+
+//   return <div className='e-food mt-20'>
+
+
+//   </div>
+// }
+
+// export default AdditivesList
+
+
+{/* <Link href={"backLink"} className='flex flex-row justify-between items-center'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ml-4 hover:ml-3 duration-200 font-bold">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
+          <span>Additives</span>
+        </Link> */}
