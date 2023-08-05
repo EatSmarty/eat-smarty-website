182d55a7f748f6500fb7ba9d10119ba0d824e432
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-05 15:06:34 +0330

    set clsx style to main

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Navigation from '@/app/page'
-import { FC } from 'react'
 import Header from '../header'
 import Category from './category'
 import Banner from './banner'
@@ -9,13 +8,17 @@ import Slogan from './slogan'
 import Features from './features'
 import Footer from './footer'
 import Why from './why'
+import clsx from 'clsx'
 
 export default function page() {
   return (
     <div className='home-screen'>
       <Navigation></Navigation>
       <Header title="home"></Header>
-      <main className='main-content flex justify-center items-center flex-col'>
+      <main className={clsx(
+        "main-content",
+        "flex justify-center items-center flex-col"
+      )}>
         <Banner></Banner>
         <Slogan></Slogan>
         <Why></Why>
