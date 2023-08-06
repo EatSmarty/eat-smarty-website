73e7495e32d94635c6d7161631931c44b6f9a573
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-06 20:45:03 +0330

    feat (page) : update page.tsx

---
+++ src/app/additives/e-number/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { FC } from 'react'
+import Header from './header'
+import Desc from './e-number'
+
+interface pageProps {
+
+}
+
+const page: FC<pageProps> = ({ }) => {
+  return (
+    <div className='home-screen'>
+      <Header></Header>
+      <main className='flex justify-center items-center'>
+        <div>
+          <Desc></Desc>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default page
