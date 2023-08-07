499083a307183958e4315f5a71c709c783206b4a
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-07 19:46:44 +0330

    feat (metadata): add title in metadata to scan page

--- src/app/scan/layout.tsx
+++ src/app/scan/layout.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next"
 import Header from "../header"
+
+export const metadata: Metadata = {
+    title: "Scan"
+}
 
 export default function DashboardLayout({
     children,
