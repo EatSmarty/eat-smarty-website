ed0440d0e7acf928ef139b0752c875bd1a407673
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-12 19:21:36 +0330

    fix (file tsx): update things

--- src/app/additives/page.tsx
+++ src/app/additives/page.tsx
@@ -3,23 +3,27 @@
 import clsx from "clsx"
 import AdditiveList from "./additivesList"
 import Search from "./search/search"
+import Header from "../header"
 
 
 export default function Page() {
   return (
-    <div className={clsx(
-      "home-screen",
-    )}>
-      <main className={clsx(
-        "main-content",
-        "mt-5 mb-14",
-        "flex justify-center items-center flex-col"
+    <>
+      <Header title={'Additives'} />
+      <div className={clsx(
+        "home-screen",
       )}>
-        <div>
-          <Search></Search>
-          <AdditiveList></AdditiveList>
-        </div>
-      </main>
-    </div>
+        <main className={clsx(
+          "main-content",
+          "mt-5 mb-14",
+          "flex justify-center items-center flex-col"
+        )}>
+          <div>
+            <Search />
+            <AdditiveList />
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
