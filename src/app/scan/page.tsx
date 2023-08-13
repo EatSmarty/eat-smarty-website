877c27b01cc5625146c7ff2ed19a5942f786d294
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-13 18:30:46 +0330

    update scanner

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -4,6 +4,7 @@ import { FC } from 'react'
 import Header from '../header'
 import Navigation from '../page'
 import BarcodeScanner from './barcodeScanner'
+// import Scan from './scan'
 
 interface pageProps {
 
