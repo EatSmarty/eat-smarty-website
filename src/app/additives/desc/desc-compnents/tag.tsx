9951175da5830a1f6322cc13c7fc2a9efac6e134
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-13 18:28:10 +0330

    feat (tag): create tag component

---
+++ src/app/additives/desc/desc-compnents/tag.tsx
@@ -0,0 +1,47 @@
+import clsx from "clsx";
+
+
+export default function Tag() {
+    return (
+        <div className={clsx(
+            "tag",
+            "text-xs",
+        )}>
+            <p className={clsx(
+                "py-1 px-2 m-1 mb-2",
+                "inline-block",
+                "bg-[#4ade80]",
+                "rounded-full",
+                "text-[#fff]"
+            )}>E-100</p>
+            <p className={clsx(
+                "py-1 px-2 m-1 mb-2",
+                "inline-block",
+                "bg-[#4ade80]",
+                "rounded-full",
+                "text-[#fff]"
+            )}>Curcumin</p>
+            <p className={clsx(
+                "py-1 px-2 m-1 mb-2",
+                "inline-block",
+                "bg-[#4ade80]",
+                "rounded-full",
+                "text-[#fff]"
+            )}>Turmeric</p>
+            <p className={clsx(
+                "py-1 px-2 m-1 mb-2",
+                "inline-block",
+                "bg-[#4ade80]",
+                "rounded-full",
+                "text-[#fff]"
+            )}>Turmeric extract</p>
+            <p className={clsx(
+                "py-1 px-2 m-1 mb-2",
+                "inline-block",
+                "bg-[#4ade80]",
+                "rounded-full",
+                "text-[#fff]"
+            )}>Curcumin powder</p>
+        </div>
+    )
+}
