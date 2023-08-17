5f4ee38e5905d36c0cae00b7c40757d24cf9a50c
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-17 18:27:17 +0330

    feat (additive list): create additive component to show additives of list

--- src/app/additives/additivesList.tsx
+++ src/app/additives/additivesList.tsx
@@ -1,37 +1,12 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import additives from './additives.json'
-import AdditiveCart from './additivesCart'
+import AdditiveCart from './additivesCart';
+import FilterdSearch from './filterdSearch';
 
-export default function AdditiveList() {
-    // const [filteredList, setFilteredList] = useState(additives.map((index) => {
-    //     return index.eNumber
-    // }))
-    // const filterBySearch = (event) => {
-    //     // Access input value
-    //     const query = event.target.value;
-    //     // Create copy of item list
-    //     var updatedList = [...filteredList]
-    //     // Include all elements which includes the search query
-    //     updatedList = updatedList.filter((item) => {
-    //         return item.toLowerCase().indexOf(query.toLowerCase()) !== -1
-    //     });
-    //     // Trigger render with updated values
-    //     setFilteredList(updatedList)
-    // }
 
-    const jsonData = additives
+export default function AdditivesList() {
 
     return (
-        jsonData.map((index) => {
-            return (
-                <AdditiveCart
-                    name={index.name}
-                    enumber={index.eNumber}
-                    badge={index.isHalal}
-                    danger={index.danger}
-                    source={index.source}
-                />
-            )
-        })
+        <FilterdSearch additives={additives} />
     )
 }
