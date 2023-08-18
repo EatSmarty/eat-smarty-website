ef7c0e56e1177a49444fa2f569a2618492120a03
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-18 19:56:19 +0330

    feat (additives card): set additives card components storybook

--- src/stories/AdditivesCard.stories.ts
+++ src/stories/AdditivesCard.stories.ts
@@ -20,13 +20,19 @@ type Story = StoryObj<typeof meta>;
 // type Story = StoryObj<typeof meta/>
 
 export const HalalCard: Story = {
-
+  args: {
+    backgroundColor: '#DCFCE7',
+  }
 };
 
 export const HaramCard: Story = {
-
+  args: {
+    backgroundColor: '#FEE2E2'
+  }
 };
 
 export const MushboohCard: Story = {
-
+  args: {
+    backgroundColor: '#FFEDD5'
+  }
 };
