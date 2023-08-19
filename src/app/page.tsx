1ab0b31fd156592eb1350b1ce4c2742bff6a9a94
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-19 21:50:40 +0330

    modify (page): del why comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Category from "../../components/home/category"
 import Features from "../../components/home/features"
 import CopyRight from "../../components/home/copyright"
 import Slogan from "../../components/home/slogan"
-import Why from "../../components/home/why"
 import Banner from "../../components/home/banner"
 
 
@@ -13,12 +12,10 @@ export default function page() {
   return (
     <>
       <main className={clsx(
-        "main-content",
         "flex justify-center items-center flex-col",
       )}>
         <Banner></Banner>
         <Slogan></Slogan>
-        <Why></Why>
         <Features></Features>
         <Category></Category>
         <CopyRight></CopyRight>
