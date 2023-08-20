6379e87e07a23c1c6494f0d09230e34bcd6535b0
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-20 23:43:20 +0330

    create (button navigate): set spread component

--- src/app/navigationbuttons.tsx
+++ src/app/navigationbuttons.tsx
@@ -3,23 +3,30 @@ import Image from 'next/image';
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+
 interface NavigationButtonsProps {
-    iconbutton?: string | null
+    /**
+   * Is this the principal set icon navigation buttons
+   */
+    icon?: string | null | undefined
+
     pathnameProp?: string | null
 }
 
-export default function Navigationbuttons({ iconbutton, pathnameProp }: NavigationButtonsProps) {
+export default function Navigationbuttons({ icon, pathnameProp }: NavigationButtonsProps) {
     const pathname = usePathname();
 
     return (
         <Link className={clsx(
             pathname === `/${pathnameProp}` ? 'active' : '',
+            "flex justify-center items-center",
+
         )} href={`./${pathnameProp}`} >
             <Image
-                src={`./svg/${iconbutton}.svg`}
+                src={`${icon}`}
                 height={28}
                 width={28}
-                alt={`${iconbutton}`}
+                alt={`${icon}`}
             />
         </Link>
     )
