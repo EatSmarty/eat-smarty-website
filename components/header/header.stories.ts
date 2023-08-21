55f182a6348c44a2e5a4beab684fc79a7b6883f9
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-21 18:55:38 +0330

    add (story): file header stories comp to components

---
+++ components/header/header.stories.ts
@@ -0,0 +1,42 @@
+import type {Meta, StoryObj} from '@storybook/react'
+import Header from './header'
+import './globals.css'
+
+const meta = {
+    title: 'Header',
+    component: Header,
+    parameters: {
+        layout: 'centerd',
+    },
+    tags: ['outodocs'],
+    argTypes: {
+
+    }
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const HeaderHome: Story = {
+    args: {
+        title: 'Home'
+    }
+}
+
+export const HeaderAdditives: Story = {
+    args: {
+        title: 'Additives'
+    }
+}
+
+export const HeaderScan: Story = {
+    args: {
+        title: 'Scan'
+    }
+}
+
+export const HeaderSetting: Story = {
+    args: {
+        title: 'Setting'
+    }
+}
