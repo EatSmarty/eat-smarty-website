7351131eee5ad1ffd8c61cd4f183f1060500e43d
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-21 20:31:10 +0330

    fix (grid to cards)

--- src/app/additives/search.tsx
+++ src/app/additives/search.tsx
@@ -1,22 +1,27 @@
+import clsx from "clsx"
 import AdditiveCart from "../../../components/additives/additivesCard"
 
 
 export default function Search({ filteredAdditives }) {
   const filtered = filteredAdditives?.map(additive =>
     <AdditiveCart
-      key={additive.code}
+      key={additive.id}
       isHalal={additive.isHalal}
       enumber={additive.eNumber}
       name={additive.name}
       source={additive.source}
       danger={additive.danger}
-    />
+      code={0} />
   )
 
   return (
-    <>
+    <div className={clsx(
+      "additives-list",
+      "grid grid-cols-4 gap-10",
+      "mb-20"
+    )}>
       {filtered}
 
-    </>
+    </div>
   )
 }
