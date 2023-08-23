3a8140457a7354cc28db5d55985b05e11b209297
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-23 20:13:53 +0330

    fix (header directory)

--- app/navigation.tsx
+++ app/navigation.tsx
@@ -1,11 +1,6 @@
 'use client'
 import clsx from 'clsx';
 import Navigationbuttons from '@/components/navigation/navigationbuttons';
-import home from '@/assets/svg/home.svg'
-import list from '@/assets/svg/list.svg'
-import scan from '@/assets/svg/scan.svg'
-import setting from '@/assets/svg/setting.svg'
-
 
 
 export default function Navigation() {
@@ -14,21 +9,17 @@ export default function Navigation() {
     return (
         <nav className={clsx(
             "flex justify-between items-center",
-            "fixed bottom-0",
             "overflow-hidden",
             "border-t",
-            "bg-white-500",
-            "navigation-bottom",
             "w-full",
-            "px-5"
+            "navigation-bottom"
         )}>
 
-            <Navigationbuttons icon={home} pathnameProp={''} />
-            <Navigationbuttons icon={list} pathnameProp={'additives'} />
-            <Navigationbuttons icon={scan} pathnameProp={'scan'} />
-            <Navigationbuttons icon={setting} pathnameProp={'setting'} />
+            <Navigationbuttons icon={'../../assets/svg/home.svg'} pathnameProp={'/'} />
+            <Navigationbuttons icon={'../../assets/svg/list.svg'} pathnameProp={'additives'} />
+            <Navigationbuttons icon={'../../assets/svg/scan.svg'} pathnameProp={'scan'} />
+            <Navigationbuttons icon={'../../assets/svg/setting.svg'} pathnameProp={'setting'} />
 
         </nav>
     )
 }
-
