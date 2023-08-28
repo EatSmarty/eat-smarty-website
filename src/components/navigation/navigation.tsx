58be9c4066f52cb368dea2c6a80f0e3d86143208
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-28 20:35:35 +0330

    import (svg comp)

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,11 +1,12 @@
 import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link'
-import home from '../../assets/svg/home.svg'
 import list from '../../assets/svg/list.svg'
 import scan from '../../assets/svg/scan.svg'
 import setting from '../../assets/svg/setting.svg'
 import Homeicon from '../svg/homicon';
+import Additivesicon from '../svg/additivesicon';
+import Scanicon from '../svg/scanicon';
 
 export default function Navigation() {
 
@@ -19,27 +20,14 @@ export default function Navigation() {
             "w-full",
             "navigation-bottom"
         )}>
-            {/* home */}
             <Link href={'/'} >
                 <Homeicon />
             </Link>
-            {/* list */}
             <Link href={'/additives'} >
-                <Image
-                    src={list}
-                    height={28}
-                    width={28}
-                    alt='icon'
-                />
+                <Additivesicon />
             </Link>
-            {/* scan */}
             <Link href={'/scan'} >
-                <Image
-                    src={scan}
-                    height={28}
-                    width={28}
-                    alt='icon'
-                />
+                <Scanicon />
             </Link>
             {/* setting */}
             <Link href={'/setting'} >
