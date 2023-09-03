c2fa898bfb740212f294ead24d9190fc59ba8272
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-03 14:42:43 +0330

    create (variable): Variables used in the project

---
+++ src/utilise/varient.tsx
@@ -0,0 +1,18 @@
+const brandName = "EATSMARTY"
+const title = "EATSMARTY"
+const keywords = [
+    "halal",
+    "haram",
+    "mushbooh",
+    "scan",
+    "scan halal",
+    "scan halal food",
+    "barcode",
+    "vegan",
+    "detect halal food",
+    "additives",
+    "open source",
+]
+
+
+export default { title, brandName, keywords }
