9ccb3bd47c8653e74e77d5974fe7bda4a35175fe
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-07 18:58:34 +0330

    fix (read barcode)

--- src/components/scan/barcodeScanner.tsx
+++ src/components/scan/barcodeScanner.tsx
@@ -2,25 +2,29 @@
 
 import React, { useState } from 'react';
 import Html5QrcodePlugin from './Html5QrcodePlugin';
+import clsx from 'clsx';
 
 const BarcodeScanner: React.FC = () => {
   const [barcode, setBarcode] = useState("")
 
   const onNewScanResult = (decodedText: string) => {
-    console.log(decodedText);
-
+    setBarcode(decodedText)
   };
 
   return (
-    <div>
-      <h1>Html5Qrcode React example!</h1>
+    <>
       <Html5QrcodePlugin
         fps={10}
         qrbox={250}
-        disableFlip={false}
+        disableFlip={true}
         qrCodeSuccessCallback={onNewScanResult}
       />
-    </div>
+      <h1 className={clsx(
+        "mt-20",
+        "text-center text-2xl text-green-600",
+        "font-bold",
+      )}>{barcode}</h1>
+    </>
   );
 };
 
