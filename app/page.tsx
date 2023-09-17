43cbb1f38500e4001d6be71933b55f9485628f45
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-17 18:48:25 +0330

    fix (err handling)

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,23 @@
+'use client'
 import clsx from "clsx"
 import Features from "@/components/home/features"
 import CopyRight from "@/components/home/copyright"
 import Slogan from "@/components/home/slogan"
 import Banner from "@/components/home/banner"
-import Searchinput from "@/components/additives/searchinput"
-
-
+import Searchbarcode from "@/components/home/searchbarcode"
 
 
 export default function page() {
-  return (
 
+  return (
     <main className={clsx(
       "flex justify-center items-center flex-col",
-
     )}>
       <Banner />
       <Slogan />
-      <Searchinput handleChange={undefined} placeholder={"what product look for..."} />
+      <Searchbarcode />
       <Features />
       <CopyRight />
     </main>
-
   )
 }
