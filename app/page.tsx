36edde3d77c035219c526d4a03b0f215cfce001c
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-30 14:14:57 +0330

    fix (header title)

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,23 @@ import CopyRight from "@/components/home/copyright"
 import Slogan from "@/components/home/slogan"
 import Banner from "@/components/home/banner"
 import Searchbarcode from "@/components/home/searchbarcode"
+import Header from "@/components/header/header"
 
 
 export default function page() {
 
   return (
-    <main className={clsx(
-      "flex justify-center items-center flex-col",
-    )}>
-      <Banner />
-      <Slogan />
-      <Searchbarcode />
-      <Features />
-      <CopyRight />
-    </main>
+    <>
+      <Header title={"Home"} />
+      <main className={clsx(
+        "flex justify-center items-center flex-col",
+      )}>
+        <Banner />
+        <Slogan />
+        <Searchbarcode />
+        <Features />
+        <CopyRight />
+      </main>
+    </>
   )
 }
