21a1d6fc2498a8af7080caf69a3a6e0a6449e97a
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-10-01 21:00:29 +0330

    feat (connect to cypress cloud)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "d98jup",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
     },
   },
 });
