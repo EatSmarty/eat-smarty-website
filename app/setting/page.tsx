2fcf79f15f34548abb3f9a76dcda094ee0900d22
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-10-01 20:45:31 +0330

    fix (header title)

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -3,6 +3,7 @@
 import { FC } from 'react'
 import About from '@/components/setting/about'
 import clsx from 'clsx'
+import Header from '@/components/header/header'
 
 interface pageProps {
 
@@ -11,16 +12,19 @@ interface pageProps {
 
 const Page: FC<pageProps> = () => {
   return (
-    <div className='home-screen mb-20'>
-      <main className={clsx(
-        "main-content",
-        "flex justify-center items-center flex-col"
-      )}>
-        <div>
-          <About></About>
-        </div>
-      </main>
-    </div>
+    <>
+      <Header title={'Setting'}></Header>
+      <div className='home-screen mb-20'>
+        <main className={clsx(
+          "main-content",
+          "flex justify-center items-center flex-col"
+        )}>
+          <div>
+            <About></About>
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
 
