dabf265018b70f61e03ce3805887153354f705fb
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-10-01 20:33:50 +0330

    fix (additive title)

---
+++ app/additives/layout.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Additives"
+}
+
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return <>
+        {children}
+    </>
+}
