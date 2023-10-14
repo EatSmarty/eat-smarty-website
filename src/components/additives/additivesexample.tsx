0ee2c68e0826e6527039254d88193b3f0eb223d7
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-10-14 22:20:52 +0330

    create (additives example)

---
+++ src/components/additives/additivesexample.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx'
+import React from 'react'
+
+export default function Additivesexample({ exampleUse }) {
+    return (
+        <div className={clsx(
+            "my-5",
+            "flex items-center"
+        )}>
+            <h4 className={clsx(
+                "text-slate-500 text-xl"
+            )}>
+                Use |
+            </h4>
+            <p className={clsx(
+                "ml-2"
+            )}>
+                {exampleUse}
+            </p>
+        </div>
+    )
+}
