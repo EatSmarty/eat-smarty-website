aceda978c1589c10a2a4d528bd7228e11ddb47dc
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-10-14 22:21:01 +0330

    update (style)

--- src/components/additives/additivesothername.tsx
+++ src/components/additives/additivesothername.tsx
@@ -4,12 +4,15 @@ import React from 'react'
 export default function Additivesothername({ othername }) {
     return (
         <div className={clsx(
-            "my-5"
+            "my-5",
+            "flex items-center"
         )}>
             <h4 className={clsx(
                 "text-slate-500 text-xl"
-            )}>other name</h4>
-            <p>
+            )}>other name | </h4>
+            <p className={clsx(
+                "mx-2"
+            )}>
                 {othername}
             </p>
         </div>
