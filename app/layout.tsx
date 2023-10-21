63531acd77945368d6236cc6d0d87094cc35e60e
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2023-10-21 16:18:46 +0330

    fix (indent)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,59 +1,58 @@
-import type { Metadata } from 'next'
-import { DM_Sans } from 'next/font/google'
-import './globals.css'
-import Navigation from '@/components/navigation/navigation'
-import varient from '@/utilise/varient'
-
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import Navigation from "@/components/navigation/navigation";
+import varient from "@/utilise/varient";
 
 const DmSans = DM_Sans({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: {
     default: `${varient.title}`,
-    template: `%s | ${varient.title}`
+    template: `%s | ${varient.title}`,
   },
-  description: 'ensuring your dietary preferences are met' +
-    'with absolute ease and confidence',
+  description: "ensuring your dietary preferences are met" +
+    "with absolute ease and confidence",
   keywords: `${varient.keywords}`,
   openGraph: {
     title: `${varient.title}`,
-    description: 'We specialize in seamlessly identifying halal food for you' +
-      'by simply scanning the products barcode',
-    url: 'https://eatsmarty.com/',
+    description: "We specialize in seamlessly identifying halal food for you" +
+      "by simply scanning the products barcode",
+    url: "https://eatsmarty.com/",
     siteName: `${varient.brandName}`,
     images: [
       {
-        url: '',
+        url: "",
         width: 800,
         height: 600,
-        alt: 'halalvar banner'
+        alt: "halalvar banner",
       },
       {
-        url: '',
+        url: "",
         width: 1800,
         height: 1600,
-        alt: 'halalvar banner',
+        alt: "halalvar banner",
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
   },
   themeColor: [
-    { media: '(default-color-scheme: light)', color: 'white' }
+    { media: "(default-color-scheme: light)", color: "white" },
   ],
   icons: {
-    icon: '/icon.ico',
-    shortcut: '/shortcut-icon.png',
-    apple: '/apple-icon.png',
+    icon: "/icon.ico",
+    shortcut: "/shortcut-icon.png",
+    apple: "/apple-icon.png",
     other: {
-      rel: 'apple-touch-icon-precomposed',
-      url: '/apple-touch-icon-precomposed.png',
-    }
+      rel: "apple-touch-icon-precomposed",
+      url: "/apple-touch-icon-precomposed.png",
+    },
   },
-}
+};
 export default function RootLayout({
   children,
 }: {
@@ -66,5 +65,5 @@ export default function RootLayout({
         <Navigation />
       </body>
     </html>
-  )
+  );
 }
