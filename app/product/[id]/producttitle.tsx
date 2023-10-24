6b4930ebe513ecb29df7a531efd1548ca1a891bd
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2023-10-24 09:14:02 +0330

    fix (product directory)

---
+++ app/product/[id]/producttitle.tsx
@@ -0,0 +1,11 @@
+import clsx from "clsx";
+import React from "react";
+
+export default function Producttitle() {
+    return (
+        <h1 className={clsx(
+            "text-2xl",
+        )}>CocaCola
+        </h1>
+    );
+}
