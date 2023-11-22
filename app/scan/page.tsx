9b39e8a1e48aa9d468a7456e9340dc2f0fc060de
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2023-11-22 08:51:25 +0330

    fix (cypress test)

--- app/scan/page.tsx
+++ app/scan/page.tsx
@@ -5,6 +5,7 @@ import clsx from "clsx";
 import Scanner from "@/components/scan/Scanner";
 import { BsUpcScan } from "react-icons/bs";
 import "./camera.css";
+import Header from "@/components/header/header";
 
 export default function Page() {
   const [camera, setCamera] = useState(false);
@@ -16,29 +17,32 @@ export default function Page() {
   };
   return (
     <>
-      <div className={clsx(
-        "flex items-center flex-col",
-        // "w-full h-full",
-        "camera",
-      )}>
+      <Header title={"Scan"} />
+      <main>
         <div className={clsx(
-          "flex justify-center items-center",
-          "rounded",
+          "flex items-center flex-col",
+          // "w-full h-full",
+          "camera",
         )}>
-          {camera && <Scanner onDetected={onDetected} />}
+          <div className={clsx(
+            "flex justify-center items-center",
+            "rounded",
+          )}>
+            {camera && <Scanner onDetected={onDetected} />}
+          </div>
+          <button
+            className="button-tap-camera"
+            onClick={() => setCamera(!camera)}
+          >
+            {camera ? "Stop" : <BsUpcScan size={150} />}
+            <p className={clsx(
+              "font-bold",
+              "text-2xl",
+              "mt-5",
+            )}>{result ? result : "Tap To Scan"}</p>
+          </button>
         </div>
-        <button
-          className="button-tap-camera"
-          onClick={() => setCamera(!camera)}
-        >
-          {camera ? "Stop" : <BsUpcScan size={150} />}
-          <p className={clsx(
-            "font-bold",
-            "text-2xl",
-            "mt-5",
-          )}>{result ? result : "Tap To Scan"}</p>
-        </button>
-      </div>
+      </main>
     </>
   );
 }
