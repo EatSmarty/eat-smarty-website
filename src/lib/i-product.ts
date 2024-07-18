15431d3c9fc477125262b20c411f649ca0f525f8
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-07-18 23:07:07 +0330

    fix product interface

--- src/lib/i-product.ts
+++ src/lib/i-product.ts
@@ -2,17 +2,17 @@ import type { ICertificates } from './i-certificates';
 import type { INutrition } from './i-nutrition';
 
 export interface IProduct {
-  id: number;
-  barcode: string;
-  imgproduct: string;
-  title: string;
-  category: string;
-  source: string;
-  description: string;
-  src: string;
-  rating: number;
-  what: string;
-  nutrition: INutrition;
-  ingradient: string[];
+  id?: number;
+  barcode?: string;
+  imgproduct?: string;
+  title?: string;
+  category?: string;
+  source?: string;
+  description?: string;
+  src?: string;
+  rating?: number;
+  what?: string;
+  nutrition?: INutrition;
+  ingradient?: string[];
   certificates?: ICertificates;
 }
