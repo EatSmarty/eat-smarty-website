67f7947a3e3c37cb489fc65856e4f3be8deebec1
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-10-28 15:34:34 +0330

    update dir product

--- app/product/[id]/product-id.tsx
+++
@@ -1,8 +0,0 @@
-interface ProductProps {
-  params: { id: number }
-}
-
-export default function Product({ params }: ProductProps) {
-  const paramsid = params.id;
-  return ({ paramsid });
-}
--- app/product/page.tsx
+++
@@ -1,7 +0,0 @@
-import Category from "@/components/home/category";
-
-export default function page({ params }) {
-  return (
-    <Category />
-  );
-}
